485927bd4056fcf1e634a3b88e1264313eaac120
DansLean <eudaniellcsilva@gmail.com>
2024-01-29 14:52:37 -0300

    feat: add splash screen

---
+++ src/screens/Login/Splash.tsx
@@ -0,0 +1,32 @@
+import React, { useEffect } from "react";
+import { View, StyleSheet } from "react-native";
+import { useNavigation } from '@react-navigation/native';
+import { StackTypes } from '../../routes/Stack';
+import Logo from "../../assets/Logo.svg";
+
+
+
+const Splash = () => {
+    const navigation = useNavigation<StackTypes>();
+
+    useEffect(() => {
+        setTimeout( () => {
+            navigation.navigate("Login");
+        }, 2000);
+    });
+    return(
+        <View style={style.container}>
+            <Logo width={"50%"} height={"50%"} />
+        </View>
+    )
+}
+
+const style = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+})
+
+export default Splash;
