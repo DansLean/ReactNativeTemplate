30dfc029171e02f982e355ef280f6c99ebab27c4
DansLean <eudaniellcsilva@gmail.com>
2024-03-17 17:04:23 -0300

    feat: exporting components and assets for an easy access

---
+++ src/assets/index.ts
@@ -0,0 +1,5 @@
+export { default as Back } from "./Back.svg";
+export { default as Eye } from "./Eve.svg";
+export { default as EyeOff } from "./EyeOff.svg";
+export { default as Logo } from "./Logo.svg";
+export { default as SideMenu } from "./SideMenu.svg";
