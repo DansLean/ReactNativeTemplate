fff21451985b0e4b94346b59b6f60516842bc7e4
DansLean <eudaniellcsilva@gmail.com>
2024-03-17 17:04:43 -0300

    feat: you can now have a icon in the button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,13 +3,15 @@ import { StyleSheet, Text, TouchableOpacity } from "react-native";
 
 
 type ButtonProps = {
-    text: string,
+    text?: string,
     onPress?: any,
+    icon?: any,
 }
 
 const Button = (props: ButtonProps) => {
     return(
-        <TouchableOpacity style={style.button} onPress={(e) => props.onPress(e)}>
+        <TouchableOpacity style={style.button} onPress={props.onPress ? (e) => props.onPress(e) : () => {}}>
+            { props.icon ? <props.icon style={props.text ? style.iconWithText : style.iconWithoutText} /> : <></> }
             <Text style={style.text}>{props.text}</Text>
         </TouchableOpacity>
     )
@@ -17,12 +19,18 @@ const Button = (props: ButtonProps) => {
 
 const style = StyleSheet.create({
     button: {
+        flexDirection: 'row',
         borderWidth: 1,
         padding: 10,
         backgroundColor: 'black',
         justifyContent: 'center',
         alignItems: 'center',
     },
+    iconWithText: {
+        position: 'absolute',
+        left: 10,
+    },
+    iconWithoutText: {},
     text: {
         fontSize: 20,
         color: 'white',
