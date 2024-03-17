b1851f56d5b73e6a1f5b64df0f32b6077c3881cd
DansLean <eudaniellcsilva@gmail.com>
2024-03-17 17:05:20 -0300

    chore: delete unused component

--- src/assets/SideMenuIcon.tsx
+++
@@ -1,7 +0,0 @@
-const SideMenuIcon = `
-<svg width="21" height="15" viewBox="0 0 21 15" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M0 14.6848V12.2901H21V14.6848H0ZM0 8.69799V6.30325H21V8.69799H0ZM0 2.71114V0.316406H21V2.71114H0Z" fill="white"/>
-</svg>
-`
-
-export default SideMenuIcon;
